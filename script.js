ffc87f8eac4a61535d145a501e9c3d6cc3532d80
cosiomar <intcomdig@gmail.com>
2024-08-20 17:10:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+  
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica de inicio de sesión y el registro de usuarios
+    alert('Inicio de sesión no implementado.');
+  });
+});
